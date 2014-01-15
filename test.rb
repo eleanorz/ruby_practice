6882c0f12c7ac7c1d972f69e4ca968f7b48b8491
elly <eleanorz@gmail.com>
2014-01-14 19:27:34 -0800

    final push for tuesday eve

--- test.rb
+++ test.rb
@@ -30,7 +30,6 @@ x = 'john, charles, matt, joe'
 puts x.split(',')		#splits up the array into multiple items, taking out the commas
 puts x.split(',').to_s  #sends the result into a string
 
-=end
 y = ''
 
 puts 'y is empty' if y.empty?
@@ -40,6 +39,7 @@ puts 'new line'
 
 print 'hello '
 print 'am I on a new line??' 
+=end
 
 BEGIN{
 	puts 'helo, mate'
@@ -48,6 +48,34 @@ BEGIN{
 END{
 	puts 'this should be at the end'
 }
+
+class CodingDojo 
+  @@no_of_branches = 0 
+  def initialize(id, name, address, random) 
+    @branch_id = id 
+    @branch_name = name 
+    @branch_address = address  #instance var
+    @@no_of_branches += 1 #class variable, goes on after this run
+    @local_random = random
+    puts "Created branch #{@@no_of_branches}" 
+  end 
+  def hello 
+    puts "Hello CodingDojo!" 
+  end 
+  def displayAll 
+    puts "Branch ID: %d" % @branch_id 
+    puts "Branch Name: %s" % @branch_name 
+    puts "Branch Address: %s" % @branch_address
+    puts 'here\'s a random variable' % @local_random
+  end 
+end 
+# now using above class to create objects 
+branch = CodingDojo.new(253, "SF CodingDojo", "Sunnyvale CA","whatever") 
+branch.displayAll 
+branch2 = CodingDojo.new(155, "Boston CodingDojo", "Boston MA","whatever") 
+branch2.displayAll
+branch3 = CodingDojo.new(155, "seattle CodingDojo", "bellevue, WA","whatever") 
+branch3.displayAll
 
 
 
